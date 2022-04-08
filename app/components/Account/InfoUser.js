53491ab68de2d007120f528cb4cdca4bf7281d46
Madeline-Esc <made_2500@hotmail.com>
2022-04-08 13:53:35 -0500

    storage y permitir acceso

--- app/components/Account/InfoUser.js
+++ app/components/Account/InfoUser.js
@@ -1,10 +1,35 @@
 import React from "react"
 import { StyleSheet, View, Text } from "react-native"
 import { Avatar } from "react-native-elements"
+import firebase from 'firebase'
+import * as Permissions from 'expo-permissions'
+import * as ImagePicker from 'expo-image-picker'
 
 export default function InfoUser(props){
     const {userInfo} = props
-    const {photoURL, displayName, email} = userInfo
+    const {photoURL, displayName, email, toastRef} = userInfo
+
+    const changeAvatar= async()=>{
+        const resultPermissions = await Permissions.askAsync(Permissions.CAMERA_ROLL)
+        console.log(resultPermissions.permissions.mediaLibrary)
+        const resultPermissionsCamera = resultPermissions.permissions.mediaLibrary.status
+
+        if(resultPermissionsCamera === 'denied '){
+            toastRef.current.show({
+                type:'error',
+                position: 'top',
+                text1: 'Permissions',
+                text2: 'Es necesario aceptar los permisos de la galeria',
+                visibility: 3000,
+            }); 
+        } else {
+            const result = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing:true,
+                aspect:[4,3]
+            })
+        }
+    }
+
     console.log(photoURL)
     console.log(displayName)
     console.log(email)
@@ -15,6 +40,7 @@ export default function InfoUser(props){
                 title='Made'
                 rounded
                 size='large'
+                onPress={changeAvatar}
                 containerStyle={styles.userInfoAvatar}
                 source={
                     photoURL ? {uri:photoURL} : require('../../../assets/img/avatar-default.jpg')
@@ -34,7 +60,6 @@ const styles = StyleSheet.create({
     viewUserInfo:{
         alignItems: 'center',
         justifyContent: 'center',
-        flexBasis: 'row',
         backgroundColor: '#f2f2f2',
         paddingTop: 30,
         paddingBottom: 30
